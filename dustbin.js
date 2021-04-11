bd1098a9e0e8c1997433b3417d7af382b3fb9f37
Yash-Chetwani <76811964+Yash-Chetwani@users.noreply.github.com>
2021-04-11 17:03:31 +0530

    Add files via upload

---
+++ dustbin.js
@@ -0,0 +1,67 @@
+class Dustbin
+{
+	constructor(x,y)
+	{
+		this.x=x;
+		this.y=y;
+		this.dustbinWidth=50;
+		this.dustbinHeight=80;
+		this.wallThickness=5;
+		//this.scale=0.2;
+		this.bottomBody=Bodies.rectangle(this.x, this.y, this.dustbinWidth, this.wallThickness, {isStatic:true})
+		this.leftWallBody=Bodies.rectangle(this.x-this.dustbinWidth/2, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:true})
+		
+		
+
+		this.rightWallBody=Bodies.rectangle(this.x+this.dustbinWidth/2, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:true})
+		
+		this.image = loadImage("dustbingreen.png");
+
+		World.add(world, this.bottomBody)
+		World.add(world, this.leftWallBody)
+		World.add(world, this.rightWallBody);
+
+	}
+	display()
+	{
+			var posBottom=this.bottomBody.position;
+			var posLeft=this.leftWallBody.position;
+			var posRight=this.rightWallBody.position;
+
+			
+
+			push()
+			translate(posLeft.x, posLeft.y);
+			//imageMode(CENTER)
+			strokeWeight(4);
+			angleMode(RADIANS)
+			//fill(255)
+			rotate(this.angle)
+			//rect(0,0,this.wallThickness, this.dustbinHeight);
+			//image(this.image, 0,0,this.r, this.r)
+			pop()
+
+			push()
+			translate(posRight.x, posRight.y);
+			//imageMode(CENTER)
+			strokeWeight(4);
+			angleMode(RADIANS)
+			//fill(255)
+			rotate(-1*this.angle)
+			//rect(0,0,this.wallThickness, this.dustbinHeight);
+			//image(this.image, 0,0,this.r, this.r)
+			pop()
+
+			push()
+			translate(posBottom.x, posBottom.y+10);
+			imageMode(CENTER)
+			strokeWeight(4);
+			angleMode(RADIANS)
+			//fill(255)
+			//rect(0,0,this.dustbinWidth, this.wallThickness);
+			image(this.image, 0,0,this.r, this.r)
+			pop()
+			
+	}
+
+}
---
+++ paper.js
@@ -0,0 +1,41 @@
+class Paper
+{
+	constructor(x,y,r)
+	{
+		var options={
+			isStatic:true,
+			restitution:0.3,
+			friction:0.5,
+			density:1.2
+			
+			}
+		this.x=x;
+		this.y=y;
+		this.r=r
+		
+		this.body=Bodies.circle(this.x, this.y, (this.r)/2, options)
+        this.image = loadImage("paper.png");
+
+		World.add(world, this.body);
+
+
+        
+
+	}
+	display()
+	{
+			
+			var paperpos=this.body.position;
+			push()
+			translate(paperpos.x, paperpos.y);
+			strokeWeight(4);
+			//fill(paper.png)
+			imageMode(CENTER);
+			image(this.image, 0,0,this.r, this.r)
+			//ellipse(0,0,this.r, this.r);
+			pop()
+			
+	}
+
+}
+
